000ff555b185fae46ec5608f7014b55cf2df5b1f
Andreja Jovanovic <ajovanov@hotmail.co.uk>
2022-11-13 11:15:45 +0100

    added FileHelper tests.

---
+++ FileHelperTests/StringTests.cs
@@ -0,0 +1,17 @@
+﻿using FileHelper;
+
+namespace FileHelperTests
+{
+    [TestFixture]
+    public class StringTests
+    {
+        [TestCase("you", "this is you and it should be you", 2)]
+        [TestCase("you", "this is You and it should be you", 2)]
+        [TestCase("not", "this is You and it should be you", 0)]
+        public void StringOccurenceFoundInText(string s, string text, int actualOccurenceCount)
+        {
+            int value = s.Occurence(text);
+            Assert.That(value, Is.EqualTo(actualOccurenceCount));
+        }
+    }
+}
