dbed4f88de07e8a64efdee6402e6959ca2030e73
Andreja Jovanovic <ajovanov@hotmail.co.uk>
2022-11-14 18:15:53 +0100

    Added implementation for UI window and ViewModel to connect to Rest API call to consume the data found.

---
+++ TextFinderClient/FinderViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Infrastructure.Client;
+using Infrastructure.Extension;
+using Infrastructure.Model;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace TextFinderClient
+{
+    public class FinderViewModel : INotifyPropertyChanged
+    {
+        public string Filename { get; set; }
+        public string TextToFind { get; set; }
+
+        public ObservableCollection<WordCount> Data { get; set; }
+
+        private readonly RestAPIClient _client;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public FinderViewModel() 
+        { 
+            _client = new RestAPIClient(); 
+            Data = new ObservableCollection<WordCount>();
+        }
+        public async Task FindText()
+        {
+            Data.Clear();
+            var text = HttpUtility.UrlEncode(TextToFind);
+            var filepath = HttpUtility.UrlEncode(Filename);
+            var requestParam = new Uri($"https://localhost:5001/api/TextFinder?Path={filepath}&Word={text}");
+            var data = await _client.GetResponseAsync(requestParam);
+            Data.AddRange<WordCount>(data);
+        }
+    }
+}
--- TextFinderClient/MainWindow.xaml.cs
+++ TextFinderClient/MainWindow.xaml.cs
@@ -1,8 +1,16 @@
-﻿using System;
+﻿using Infrastructure.Client;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Web;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,6 +20,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Path = System.IO.Path;
 
 namespace TextFinderClient
 {
@@ -20,17 +29,41 @@ namespace TextFinderClient
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow() => InitializeComponent();
-
-        private void Find_Click(object sender, RoutedEventArgs e)
+        public MainWindow()
         {
-            var t = this.TextToFind.Text;
-            MessageBox.Show("To be implemented...");
+            InitializeComponent();
+            _viewModel = new FinderViewModel();
+            this.DataContext = _viewModel;
+            listView.ItemsSource = _viewModel.Data;
+        } 
+        private readonly RestAPIClient _client = new RestAPIClient();
+        private FinderViewModel _viewModel;
+
+        private async void Find_Click(object sender, RoutedEventArgs e)
+        {
+            await _viewModel.FindText();
         }
 
         private void TextToFind_GotFocus(object sender, RoutedEventArgs e)
         {
             this.TextToFind.Text = string.Empty;
+        }
+
+        private void SelectFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = "C:\\";
+            openFileDialog.ValidateNames = false;
+            openFileDialog.CheckFileExists = false;
+            openFileDialog.CheckPathExists = true;
+            openFileDialog.Filter = "Text files (*.txt)|*.txt";
+            // Always default to Folder Selection.
+            //Note: Issue with Win32 version cuts the text in filename, so to be replaced with WinForms version.
+            openFileDialog.FileName = "Folder Selection...";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                this.FileLocation.Text = Path.GetDirectoryName(openFileDialog.FileName);
+            }    
         }
     }
 }
