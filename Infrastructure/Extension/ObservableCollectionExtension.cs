1759c9338209e52228960b8beb56a612cf253790
Andreja Jovanovic <ajovanov@hotmail.co.uk>
2022-11-14 17:57:43 +0100

    Added ObservableCollection extension method to help add more items.

---
+++ Infrastructure/Extension/ObservableCollectionExtension.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Infrastructure.Extension
+{
+    public static class ObservableCollectionExtension
+    {
+        /// <summary>
+        /// taken from https://gist.github.com/brunossn/197f0ad0820258da27f54f917d11ac49
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="lista"></param>
+        /// <param name="items"></param>
+        public static void AddRange<T>(this ObservableCollection<T> lista, IEnumerable<T> items)
+        {
+            foreach (var item in items)
+            {
+                lista.Add(item);
+            }
+        }
+    }
+}
